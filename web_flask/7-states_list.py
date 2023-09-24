0c415f5923c37bdfb170f26c1e9720afb075bfc5
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-24 17:57:38 +0100

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def teardown(e):
 @app.route("/states_list", strict_slashes=False)
 def states_list_page():
   states = storage.all(State).values()
+  states = sorted(states, key=lambda state: state.name)
   return render_template("7-states_list.html", states = states)
 
 
