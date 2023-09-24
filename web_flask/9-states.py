20d7bd9add25fcaae257f6be990f9da79a0ccb86
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-24 18:23:56 +0100

    task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""we application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """removes SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays states """
+    states = storage.all(State)
+    return render_template('9-states.html', state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays cities of specific state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
