3e40ea1f375d75bf724ba4a678c560d8e17cc320
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 00:01:17 +0100

    task 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" start flask app """
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(e):
+    """ close session then reload"""
+    storage.close()
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """ display hbnb_filters """
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port='5000', debug=True)
