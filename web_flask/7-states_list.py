b53be1994a8fe20aa9c84dd2199934a0578c286f
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-24 17:55:59 +0100

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(e):
+  storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def states_list_page():
+  states = storage.all(State).values()
+  return render_template("7-states_list.html", states = states)
+
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000, debug=True)
