570c46886be38fe854f89adac3d8feda33fabfd0
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-24 11:23:10 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-""" Hello route """
+"""Flask web application"""
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def home_route():
-    """home page controller"""
+def home_page():
+    """Home controller"""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    """hbnbh controller"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
