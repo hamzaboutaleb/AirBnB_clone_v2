d1c63394feeebe16f490988a91ae6c0809888891
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 10:41:31 +0100

    task 9

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,9 +7,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def teardown(e):
-  storage.close()
+
+@app.route("/", strict_slashes=False)
+def home_route():
+    """home page controller"""
+    return "Hello HBNB!"
 
 @app.route("/states_list", strict_slashes=False)
 def states_list_page():
@@ -17,6 +19,9 @@ def states_list_page():
   states = sorted(states, key=lambda state: state.name)
   return render_template("7-states_list.html", states = states)
 
+@app.teardown_appcontext
+def teardown(e):
+  storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
