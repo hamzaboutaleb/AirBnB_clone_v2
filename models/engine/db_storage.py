5177b796955c5915d4b3770546b05acfd6c8a414
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 10:06:16 +0100

    task 8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,7 +73,7 @@ class DBStorage():
     def close(self):
         """ close session"""
         self.__session.close()
-        self.reload()
+        self.__session = self.Session()
 
     def reload(self):
         """ Create tables and current db session """
