30bb8b9faa191df893bd0ef9ef1d1e00fa6a1697
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 09:57:17 +0100

    task 8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,7 +72,7 @@ class DBStorage():
 
     def close(self):
         """ close session"""
-        self.__session.__class__.close(self.__session)
+        self.Session.remove()
         self.reload()
 
     def reload(self):
@@ -80,5 +80,5 @@ class DBStorage():
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
                 bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.Session = scoped_session(session_factory)
+        self.__session = self.Session()
