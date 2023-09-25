4c127433e6bf2caaa000656cc85e7a4008f5c483
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 10:37:34 +0100

    task 9

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,4 +18,5 @@ def states_list_page():
   return render_template("7-states_list.html", states = states)
 
 
-app.run(host="127.0.0.1", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
