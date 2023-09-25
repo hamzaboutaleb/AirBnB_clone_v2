19486bb797838d6bf995368e29158462eefe58e3
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 11:49:20 +0100

    task 0

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-
+from models import storage
 Base = declarative_base()
 
 
